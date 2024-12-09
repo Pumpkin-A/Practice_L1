3fbc4973d8937171a3910f4fe80ef77feedfd3f2
Pumpkin-A <90926986+Pumpkin-A@users.noreply.github.com>
2024-12-09 22:27:29 +0300

    26

---
+++ task26/26.2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// Если порядок символов не важен, в решении задачи можно использовать сортировку
+// Если сортировать строку, то повторяющиеся символы окажутся рядом, и их можно легко обнаружить, сравнивая соседние символы.
+
+func areAllUnique(str string) bool {
+	str = strings.ToLower(str)
+
+	runes := []rune(str)
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+
+	// Проверяем соседние символы
+	for i := 1; i < len(runes); i++ {
+		if runes[i] == runes[i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	stringsToCheck := []string{"abcd", "abCdefAf", "aabcd", "Ррпва", "Правоыф"}
+
+	for _, str := range stringsToCheck {
+		result := areAllUnique(str)
+		fmt.Println(str, result)
+	}
+}
